9ddcfbd44559f360762c5c7e31a06d81a542dbb5
Daniel Mendez <Daniel.Mendez@ey.com>
2018-10-27 23:23:17 -0500

    initial configuration base

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import "./main.scss"
+
+const App = () => {
+    return (
+        <div>All start Here</div>
+    )
+}
+ReactDOM.render(<App />, document.getElementById("app"))
